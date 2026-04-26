0eb9ae78fbb62bef587b1ea32f7ef2b0586167de
Mountain Sea Villa <noreply@mountainseavilla.com>
2026-04-27 01:24:27 +0800

    Add multi-page SEO site structure

--- script.js
+++ script.js
@@ -26,6 +26,46 @@ const nightInput = document.getElementById("nightInput");
 const guestInput = document.getElementById("guestInput");
 const estimateOutput = document.getElementById("estimateOutput");
 
+const ensureFloatingActions = () => {
+  if (document.querySelector(".floating-actions")) {
+    return;
+  }
+
+  document.querySelector(".page-shell")?.insertAdjacentHTML(
+    "beforeend",
+    `
+      <div class="floating-actions" aria-label="快速聯絡">
+        <a class="floating-action floating-call" href="tel:+88683688295" aria-label="撥打訂房專線">
+          <svg aria-hidden="true" viewBox="0 0 24 24">
+            <path d="M6.6 10.8c1.7 3.4 3.2 4.9 6.6 6.6l2.2-2.2c.3-.3.8-.4 1.2-.3 1.3.4 2.7.6 4.1.6.7 0 1.3.6 1.3 1.3v3.5c0 .7-.6 1.3-1.3 1.3C10.4 22 2 13.6 2 3.3 2 2.6 2.6 2 3.3 2h3.5c.7 0 1.3.6 1.3 1.3 0 1.4.2 2.8.6 4.1.1.4 0 .9-.3 1.2l-1.8 2.2z" />
+          </svg>
+          <span class="sr-only">電話</span>
+        </a>
+        <a class="floating-action floating-facebook" href="https://www.facebook.com/%E8%A5%BF%E8%8E%92%E5%B1%B1%E6%B5%B7%E4%B8%80%E5%AE%B6%E6%9C%83%E9%A4%A8-1983491898558309/" target="_blank" rel="noreferrer" aria-label="前往山海一家 Facebook">
+          <svg aria-hidden="true" viewBox="0 0 24 24">
+            <path d="M14.3 8.1V6.6c0-.7.5-.9.9-.9h2.2V2h-3.1c-3.4 0-4.8 2.1-4.8 4.7v1.4H6.8v3.8h2.7V22h4.8V11.9h3.2l.5-3.8h-3.7z" />
+          </svg>
+          <span class="sr-only">Facebook</span>
+        </a>
+        <a class="floating-action floating-line" href="https://line.me/ti/p/~0972098380" target="_blank" rel="noreferrer" aria-label="加入山海一家 Line">
+          <svg aria-hidden="true" viewBox="0 0 24 24">
+            <path d="M20.6 10.4c0-4.1-4.1-7.4-9.1-7.4s-9.1 3.3-9.1 7.4c0 3.7 3.3 6.8 7.8 7.3.3.1.7.2.8.5.1.3.1.6 0 .9l-.1.8c-.1.3-.2 1.1.8.6s5.4-3.2 7.4-5.4c1-1.2 1.5-2.9 1.5-4.7zM8.2 12.8H6.4c-.2 0-.4-.2-.4-.4V8.6c0-.2.2-.4.4-.4s.4.2.4.4V12h1.4c.2 0 .4.2.4.4s-.2.4-.4.4zm1.6-.4c0 .2-.2.4-.4.4s-.4-.2-.4-.4V8.6c0-.2.2-.4.4-.4s.4.2.4.4v3.8zm4.4 0c0 .2-.1.3-.3.4h-.1c-.1 0-.2-.1-.3-.2l-1.9-2.6v2.4c0 .2-.2.4-.4.4s-.4-.2-.4-.4V8.6c0-.2.1-.3.3-.4.2-.1.4 0 .5.2l1.9 2.6V8.6c0-.2.2-.4.4-.4s.4.2.4.4v3.8zm3.2-2.3c.2 0 .4.2.4.4s-.2.4-.4.4h-1.4V12h1.4c.2 0 .4.2.4.4s-.2.4-.4.4h-1.8c-.2 0-.4-.2-.4-.4V8.6c0-.2.2-.4.4-.4h1.8c.2 0 .4.2.4.4s-.2.4-.4.4h-1.4v1.1h1.4z" />
+          </svg>
+          <span class="sr-only">Line</span>
+        </a>
+        <a class="floating-action floating-map" href="https://share.google/b4YWjxrqz31nvUyzh" target="_blank" rel="noreferrer" aria-label="開啟 Google 地圖導航">
+          <svg aria-hidden="true" viewBox="0 0 24 24">
+            <path d="M12 2a7.5 7.5 0 0 0-7.5 7.5c0 5.2 6.5 11.8 6.8 12.1.4.4 1 .4 1.4 0 .3-.3 6.8-6.9 6.8-12.1A7.5 7.5 0 0 0 12 2zm0 10.2a2.7 2.7 0 1 1 0-5.4 2.7 2.7 0 0 1 0 5.4z" />
+          </svg>
+          <span class="sr-only">Google 地圖</span>
+        </a>
+      </div>
+    `
+  );
+};
+
+ensureFloatingActions();
+
 const heroSlides = [
   {
     image: "./assets/images/banner-1.jpg",
